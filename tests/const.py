c99e2001fe13de487346aaa0223985dfb192c1c4
Åke Strandberg <ake@strandberg.eu>
2022-12-01 11:46:39 +0000

    WIP Add tests

---
+++ tests/const.py
@@ -0,0 +1,5 @@
+"""Constants for viva tests."""
+# from custom_components.viva.const import CONF_PASSWORD, CONF_USERNAME
+
+# Mock config data to be used across multiple tests
+MOCK_CONFIG = {"id": "12"}
